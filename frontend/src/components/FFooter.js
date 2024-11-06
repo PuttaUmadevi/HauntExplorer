c4ade67f59ba5a44cbfd06aba87c4f7137238905
PuttaUmadevi <puttaumadevireddy789@gmail.com>
2024-11-06 18:54:38 +0530

    adding frontend folder

---
+++ frontend/src/components/FFooter.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function FFooter(){
+    return (
+        <footer>
+  <div className="footer-container">
+    <div className="footer-column">
+      <h3>Explore Haunt Explorer</h3>
+      <ul>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Careers</a></li>
+        <li><a href="#">Press Releases</a></li>
+        <li><a href="#">Haunt Explorer Science</a></li>
+      </ul>
+    </div>
+    <div className="footer-column">
+      <h3>Connect with Us</h3>
+      <ul>
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Instagram</a></li>
+        <li><a href="#">YouTube</a></li>
+      </ul>
+    </div>
+    <div className="footer-column">
+      <h3>Make Money with Us</h3>
+      <ul>
+        <li><a href="#">Sell on Haunt Explorer</a></li>
+        <li><a href="#">Become an Affiliate</a></li>
+        <li><a href="#">Advertise Your Products</a></li>
+        <li><a href="#">Self-Publish with Us</a></li>
+      </ul>
+    </div>
+    <div className="footer-column">
+      <h3>Let Us Help You</h3>
+      <ul>
+        <li><a href="#">Your Account</a></li>
+        <li><a href="#">Your Orders</a></li>
+        <li><a href="#">Shipping Rates &amp; Policies</a></li>
+        <li><a href="#">Returns &amp; Replacements</a></li>
+        <li><a href="#">Help</a></li>
+      </ul>
+    </div>
+  </div>
+  <div className="footer-bottom">
+    <p>© 2023 Haunt Explorer. All rights reserved.</p>
+  </div>
+</footer>
+
+    )
+}
+
+export default  FFooter;
